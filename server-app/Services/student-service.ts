c089e962efc41232d430b16db8c0307517682fb9
prg.abdullah <prg.abdullah@gmail.com>
2024-08-01 13:18:47 +0500

    code refactored to MVC pattern

---
+++ server-app/Services/student-service.ts
@@ -0,0 +1,51 @@
+import { StudentModel } from "../Database/Models/student-model";
+import { IStudent } from '../Database/Interfaces/IStudent';
+
+class StudentService {
+    async getStudents() {
+        try {
+            let students = await StudentModel.find({});
+            return students;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    async saveStudent(student: IStudent){
+        try {
+            const studentModel = StudentModel.build(student);
+            await studentModel.save();
+            return studentModel;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateStudent(student: IStudent){
+        try {
+            if (!student.rollNumber || isNaN(student.rollNumber)) {
+                throw new Error("Invalid roll number.");
+              }
+              //updating student
+              const studentModel = await StudentModel.findOneAndUpdate({rollNumber: student.rollNumber }, student, { new: true });
+              if (!studentModel) {
+                throw new Error("student not found.");
+              }
+              return studentModel;
+        } catch (error) {
+            throw error;           
+        }
+    };
+
+    async deleteStudent(rollNumber: number){
+        try {
+            const deletedCount = await StudentModel.deleteOne({rollNumber: rollNumber});
+            return { deletedCount: deletedCount };
+        } catch (error) {
+            throw error;           
+        }
+    };
+}
+
+export const studentService = new StudentService();
+
